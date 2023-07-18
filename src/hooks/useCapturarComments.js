f63a3db2271ef191a8e54fb55522fe161d9b9570
Felipeferrazamaral <felipeferrazamaral@gmail.com>
2023-07-17 21:09:46 -0300

    att-trabalho

---
+++ src/hooks/useCapturarComments.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../constants/constants";
+import axios from "axios";
+
+
+
+const useCapturarComments = () => {
+
+
+
+    const [postagens, setPostagens] = useState([]);
+
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}comments`)
+      .then((response) => {
+        setPostagens(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
+  return postagens
+
+
+
+
+
+}
+
+
+export default useCapturarComments
